f9acabe7700cf235d9efff95983229ab161e5d23
Daniel Otavio Todt <danieltodt@gmail.com>
2017-06-26 22:31:21 -0300

    service, consumer and producer, first try.

---
+++ apps/calc-prc/src/service/operations.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/**
+ * Sum two numbers.
+ * @param a
+ * @param b
+ */
+export const sum = (a, b) => a + b;
+/**
+ * Subtract two numbers.
+ * @param a
+ * @param b
+ */
+export const subtract = (a, b) => a - b;
+/**
+ * Divide two numbers.
+ * @param a
+ * @param b
+ */
+export const divide = (a, b) => a / b;
+/**
+ * Multiply two numbers.
+ * @param a
+ * @param b
+ */
+export const multiply = (a, b) => a * b;
---
+++ apps/calc-prc/src/transport/consumer.js
@@ -0,0 +1,49 @@
+'use strict';
+
+/**
+ * Imports.
+ */
+const Kafka = require('kafka-node'),
+    Consumer = Kafka.Consumer,
+    Client = Kafka.Client;
+
+/**
+ * Kafka zookeeper address.
+ */
+const broker = process.env.BROKER_ADDRESS || '192.168.50.213:2181';
+
+/**
+ * Zookeeper client.
+ */
+const client = new Client(broker);
+
+/**
+ * Queue settings.
+ */
+const topic = 'calculator.from.ws',     // queue name
+    partition = 0,                      // default = 0, random = 1, cyclic = 2, keyed = 3, custom = 4
+    fetchRequest = [{                   // array of topics to consume
+        topic: topic,
+        partition: partition
+    }],
+    fetchOptions = {                    // consumer options
+        groupId: 'calculator.processor',
+        autoCommit: false
+    };
+
+/**
+ * Queue consumer.
+ */
+const consumer = new Consumer(client, fetchRequest, fetchOptions);
+
+consumer.on('message', message => {
+    console.log("message", message);
+});
+
+consumer.on('error', error => {
+    console.log("error", error);
+});
+
+consumer.on('offsetOutOfRange', offsetOutOfRange => {
+    console.log("offsetOutOfRange", offsetOutOfRange);
+});
