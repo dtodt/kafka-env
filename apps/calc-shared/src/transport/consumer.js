c6dd78ad88c1f4a021252dd549751058d67367d0
Daniel Otavio Todt <danieltodt@gmail.com>
2017-06-30 21:51:22 -0300

    kafka transport consumer and producer shared code.

---
+++ apps/calc-shared/src/transport/consumer.js
@@ -0,0 +1,44 @@
+'use strict';
+
+/**
+ * Imports.
+ */
+const Kafka = require('node-rdkafka');
+
+/**
+ * Kafka zookeeper address.
+ */
+const broker = process.env.BROKER_ADDRESS || '192.168.50.100:9092';
+
+const kafkaGlobalConsumerConfig = {
+    'metadata.broker.list': broker,
+    'group.id': 'kafka',
+    'socket.keepalive.enable': true,
+    'enable.auto.commit': true,
+    'auto.commit.interval.ms': 10
+};
+
+function subscribeToTopic(topics, cb) {
+
+    let stream = Kafka.KafkaConsumer.createReadStream(kafkaGlobalConsumerConfig, {}, {
+        topics: topics,
+        waitInterval: 0,
+        objectMode: false
+    });
+
+    stream.on('data', function (data) {
+        cb(data);
+    });
+
+    stream.on('error', function (err) {
+        console.log(err);
+    });
+
+    stream.consumer.on('event.error', function (err) {
+        console.log(err);
+    });
+}
+
+module.exports = {
+    subscribeToTopic: subscribeToTopic
+};
---
+++ apps/calc-shared/src/transport/producer.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * Imports.
+ */
+const Kafka = require('node-rdkafka');
+
+/**
+ * Kafka zookeeper address.
+ */
+const broker = process.env.BROKER_ADDRESS || '192.168.50.100:9092';
+
+const kafkaGlobalProducerConfig = {
+    'metadata.broker.list': broker
+};
+
+function getProducerForTopic(topicName) {
+
+    let stream = Kafka.Producer.createWriteStream(kafkaGlobalProducerConfig, {}, {
+        topic: topicName
+    });
+
+    stream.on('error', function (err) {
+        console.error('Error in our kafka stream');
+        console.error(err);
+    });
+
+    return stream;
+}
+
+module.exports = {
+    getProducerForTopic: getProducerForTopic
+};
