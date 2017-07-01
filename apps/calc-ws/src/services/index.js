cd8e35125c3b1a9e7e3cd756185f36b1a094415b
Daniel Otavio Todt <danieltodt@gmail.com>
2017-07-01 10:59:48 -0300

    operations changed.

--- apps/calc-ws/src/services/index.js
+++ apps/calc-ws/src/services/index.js
@@ -1,10 +1,45 @@
 'use strict';
 
+const addProducer = require('../transport/producer').getProducerForTopic('calculator.to.add'),
+    subProducer = require('../transport/producer').getProducerForTopic('calculator.to.sub'),
+    addConsumer = require('../transport/consumer'),
+    subConsumer = require('../transport/consumer');
+
+const add = (args, callback) => {
+    let operation = {
+        x: Number(args.x),
+        y: Number(args.y)
+    };
+
+    addProducer.write(new Buffer(JSON.stringify(operation)));
+
+    addConsumer.subscribeToTopic('calculator.from.add', (data) => {
+        console.log('calculator.from.add: ' + data.toString());
+
+        callback(data.toString());
+    });
+};
+
+const sub = (args, callback) => {
+    let operation = {
+        x: Number(args.x),
+        y: Number(args.y)
+    };
+
+    subProducer.write(new Buffer(JSON.stringify(operation)));
+
+    subConsumer.subscribeToTopic('calculator.from.sub', (data) => {
+        console.log('calculator.from.sub: ' + data.toString());
+
+        callback(data.toString());
+    });
+};
+
 const services = {
     calculator: {
         calculatorService: {
-            add: require('./add'),
-            sub: require('./sub')
+            add: add,
+            sub: sub
         }
     }
 };
